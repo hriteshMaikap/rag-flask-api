72d30a3e35a6b7cb5a97be8e37cccf0266caa858
hriteshMaikap <hriteshkumarmaikap@gmail.com>
2024-08-10 22:24:25 +0530

    Added Groq API for faster generation

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ class Config:
     QDRANT_URL = os.getenv("QDRANT_URL")
     QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
     COLLECTION_NAME = "question_answer_collection"
-    MODEL_NAME = "mistral"
+    #MODEL_NAME = "llama3-70b-8192"  # Related to the retrieval part
     DEBUG = os.getenv("DEBUG", "False").lower() in ['true', '1', 't']
+    #GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 config = Config()
